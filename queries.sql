0d141d259b0f760c2dcf7e72d6f333b0a1e6dc5b
JuanJSoto <JJ3185@hotmail.com>
2022-10-14 23:14:02 -0400

    updated application.properties to have updated password

--- queries.sql
+++ queries.sql
@@ -1,7 +1,16 @@
 -- Part 1: Test it with SQL
+SHOW COLUMNS FROM job;
 
 -- Part 2: Test it with SQL
+SELECT name from techjobs.employer
+WHERE (location = "St. Louis") OR (location = "Saint Louis");
 
 -- Part 3: Test it with SQL
+DROP TABLE techjobs.job;
 
 -- Part 4: Test it with SQL
+SELECT techjobs.skill.name, techjobs.skill.description
+FROM skill
+LEFT JOIN job_skills ON skill.id = job_skills.skills_id
+WHERE jobs_id IS NOT NULL
+ORDER BY skill.name;
